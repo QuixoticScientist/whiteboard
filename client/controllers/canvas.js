31d583e3422abdf62ec5119274a582af7868df2d
Haley Bash <halebash@umich.edu>
2015-12-04 12:10:50 -0800

    Go from 2 to 10 random rooms

--- client/controllers/canvas.js
+++ client/controllers/canvas.js
@@ -3,18 +3,13 @@ angular.module('whiteboard.canvas', [])
   .controller('CanvasController', function ($scope, Canvas) {
     $scope.board = {};
 
-    // var socket = io.connect('http://localhost:4000');
-    // socket.on('test', function (data) {
-    //   console.log(data);
-    // });
-
     var socket_connect = function (room) {
       return io('localhost:4000', {
           query: 'r_var='+room
       });
     }
 
-    var random_room = Math.floor((Math.random() * 2) + 1);
+    var random_room = Math.floor((Math.random() * 10) + 1);
     var socket      = socket_connect(random_room);
 
     socket.emit('chat message', 'hello room #'+random_room);

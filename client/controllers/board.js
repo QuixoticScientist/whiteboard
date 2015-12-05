b806442b749e7c9e7a51b2fcfe16fbe1bf52b6da
Lorenzo De Nobili <elledienne@gmail.com>
2015-12-04 17:30:55 -0800

    Implement main Board controller

---
+++ client/controllers/board.js
@@ -0,0 +1,19 @@
+angular.module('whiteboard.board', [])
+
+.controller('BoardCtrl', function ($scope, Board, Draw) {
+  // Create new board 
+  Board.createBoard('board-container', 400, 300);
+  // start listening to mouse events
+  Board.attachMouseListeners(Draw.startShape);
+  // Store some data in the Board factory
+  
+
+  Board.canvasX = Board.$el.position().left;
+  Board.canvasY = Board.$el.position().top;
+  
+  // Select tool
+  Board.tool = {
+    name: "createRectangle",
+    fill: "red"
+  };
+});

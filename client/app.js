bbd6a6e50f44a04f4cca1ab3a04d09eee62b07f8
Lorenzo De Nobili <elledienne@gmail.com>
2015-12-04 19:27:08 -0800

    Add auth

--- client/app.js
+++ client/app.js
@@ -2,19 +2,34 @@ angular.module('whiteboard', [
   'whiteboard.board',
   'whiteboard.services.board',
   'whiteboard.services.draw',
+  'whiteboard.services.auth',
+  'whiteboard.services.token',
   'ngRoute'
 ])
-.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
+.config(['$routeProvider', '$locationProvider', '$httpProvider',
+  function($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'board.html',
+        templateUrl: 'views/board.html',
         controller: 'BoardCtrl',
-        controllerAs: 'board'
+        controllerAs: 'board',
+        authenticate: true
       });
+
+    //$httpProvider.interceptors.push('AttachTokens');
 
     $locationProvider.html5Mode({
       enabled: true,
       requireBase: false
     });
-}]);
+}])
+.run(function ($rootScope, $location, Auth) {
+  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      Auth.retrieveToken()
+        .then(function (token) {
+          Auth.storeToken(token);
+        })
+    }
+  });
+});

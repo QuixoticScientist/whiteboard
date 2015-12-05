f1fb1e960fd756a8f18c04993475f07b4a6f1e37
Lorenzo De Nobili <elledienne@gmail.com>
2015-12-04 17:31:33 -0800

    Implement Board builder/store service

---
+++ client/services/board.js
@@ -0,0 +1,28 @@
+angular.module('whiteboard.services.board', [])
+.factory('Board', function () {
+  var board = {};
+  board.$el;
+  board.paper;
+
+  board.createBoard = function (appendTo, width, height) {
+    this.paper = Raphael(document.getElementById(appendTo), width, height);
+    this.$el = $('#' + appendTo + ' svg');
+  };
+
+  board.attachMouseListeners = function (handler) {
+    var self = this;
+    
+    this.$el.on('mousedown', function (e) {
+      console.log('click')
+      handler(e);
+    });
+    
+    this.$el.on('mouseup', function () {
+      self.$el.off('mousemove');
+    });
+    
+  };
+
+  return board;
+
+});

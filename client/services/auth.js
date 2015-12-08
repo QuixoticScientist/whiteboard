ae6c377c9e2db162ea38304a4664aea62ec68f6a
Haley Bash <halebash@umich.edu>
2015-12-08 11:44:44 -0800

    Remove further reference to tokens in client side

--- client/services/auth.js
+++ client/services/auth.js
@@ -1,27 +1,6 @@
 angular.module('whiteboard.services.auth', [])
 .factory('Auth', function ($http, $window) {
 
-  var storeToken = function (token) {
-    $window.localStorage.setItem('token', token);
-  };
-
-  var retrieveToken = function () {
-    return $http({
-      method: 'GET', 
-      url: '/getToken'
-    }).then(function (res) {
-      return res.data.token;
-    })
-  };
-
-  var isAuth = function () {
-    return !!$window.localStorage.getItem('token');
-  };
-
-  var logoutUser = function() {
-    $window.localStorage.removeItem('token');
-  };
-
   var generateRandomId = function (length) {
     var id = "";
     var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -34,10 +13,6 @@ angular.module('whiteboard.services.auth', [])
   };
 
   return {
-    isAuth: isAuth,
-    logoutUser: logoutUser,
-    storeToken: storeToken,
-    retrieveToken: retrieveToken,
     generateRandomId: generateRandomId
   };
 });

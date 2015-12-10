f79c73f4104b303222e7e53f7ec56cdc8cff5268
Lorenzo De Nobili <elledienne@gmail.com>
2015-12-09 17:46:27 -0800

    Move shape smoother into separate service

---
+++ client/services/shape-manipulation.js
@@ -0,0 +1,23 @@
+angular.module('whiteboard.services.shapemanipulation', [])
+.factory('ShapeManipulation', function (ShapeBuilder, ShapeEditor) {
+
+  var pathSmoother = function (tool, pathElement) {
+    if (tool === 'path') {
+      var path = pathElement.attr('path');
+      var interval = 5;
+      var newPath = path.reduce(function (newPathString, currentPoint, index, path) {
+        if (!(index % interval) || index === (path.length - 1)) {
+          return newPathString += currentPoint[1] + ',' + currentPoint[2] + ' ';
+        } else {
+          return newPathString;
+        }
+      }, path[0][0] + path[0][1] + ',' + path[0][2] + ' ' + "R");
+      pathElement.attr('path', newPath);
+    }
+  };
+
+  return {
+    pathSmoother: pathSmoother
+  };
+
+});

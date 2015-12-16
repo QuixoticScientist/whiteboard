29f1ca87d5146a038211564fa8cd6ad5fa8f0f8c
Christian Everett <ceverett@gmail.com>
2015-12-16 11:10:43 -0800

    added grab and move shapes for leap motion

--- client/services/event-handler.js
+++ client/services/event-handler.js
@@ -25,35 +25,28 @@ angular.module('whiteboard.services.eventhandler', [])
     ShapeManipulation.moveShape(id, socketID, x, y);
   }
 
-  function cursorStart () {
-    // var cursorTool = {name: 'circle', colors: {stroke: '#ff0000', fill: '#ff0000'}};
-    // createShape(999, 'cursor', cursorTool, window.innerWidth / 2, window.innerHeight / 2);
-    // editShape(999, 'cursor', cursorTool, .5, .5);
-    // finishShape(999, 'cursor', cursorTool);
-  }
-
   function cursor (screenPosition) {
     var cursor = BoardData.getCursor() || BoardData.setCursor();
     BoardData.moveCursor(screenPosition);
   }
 
   function grabShape (screenPosition) {
-    var x = screenPosition[0];
-    var y = screenPosition[1];
-    var currentShape = BoardData.getCurrentShape();
+    var x = Math.floor(screenPosition[0]);
+    var y = Math.floor(screenPosition[1]);
 
-    var currentTool = BoardData.getCurrentTool();
-    var socketID = BoardData.getSocketID();
+    var currentEditorShape;
 
-    if (currentTool.name === 'eraser') {
-      toggleEraser();
-    } else if (currentTool.name === 'move') {
+    currentEditorShape = BoardData.getEditorShape();
+
+    if (!currentEditorShape) {
       var shape = BoardData.getBoard().getElementByPoint(x, y);
       if (shape) {
         BoardData.setEditorShape(shape);
+        currentEditorShape = BoardData.getEditorShape();
       }
+    } else {
+      moveShape(currentEditorShape.id, currentEditorShape.data('socketID'), x, y);
     }
-    console.log('grabbed shape at ', x, ': ', y)
   }
 
   return {

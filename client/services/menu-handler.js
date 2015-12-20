f8c268a42fe2c3148a25c7fdd9534a21f54e823d
Lorenzo De Nobili <elledienne@gmail.com>
2015-12-19 16:07:33 -0800

    Implement mouse event handling for marching menu

--- client/services/menu-handler.js
+++ client/services/menu-handler.js
@@ -1,9 +1,13 @@
 angular.module('whiteboard.services.menuhandler', [])
-.factory('MenuHandler', function () {
+.factory('MenuHandler', ['BoardData', function (BoardData) {
 
   var $menuOpener;
   var $firstLevelMenu;
   var $secondLevelMenu;
+
+  var menuWidth = 200; // Pixels
+  // This variable set the width of the area that trigger the 'show child menu' event
+  var activeArea = menuWidth / 2;
 
   function setToolbarElements (element) {
     $menuOpener = element.find('.menu-opener');
@@ -11,18 +15,23 @@ angular.module('whiteboard.services.menuhandler', [])
     $secondLevelMenu = element.find('.menu.second-level');
   }
 
-  function showFirstLevel (ev) {
-    // hide menuOpener
-    $menuOpener.removeClass('show');
-    // show first menu
-    $firstLevelMenu.addClass('show');
-    // attach over listener to sub levels
-  }
+  function firstLevelHandler (ev, action) {
+    action = action || 'show';
 
+    if (action === 'show') {
+      showFirstLevel();
+    } else if (action === 'hide'){
+      hideSecondLevel();
+      hideFirstLevel();
+    }
+  }
   function secondLevelHandler (ev, child) {
     var x = ev.clientX;
-    console.log(x)
-    if (x > 100) {
+    console.log(ev)
+
+    if (x < menuWidth && ev.type === 'mouseleave'){
+      hideSecondLevel(child);
+    } else if (x > activeArea) {
       // SHOw
       showSecondLevel(child);
     } else {
@@ -31,217 +40,56 @@ angular.module('whiteboard.services.menuhandler', [])
     }
   }
 
+  function selectToolHandler (ev, tool, element) {
+    //console.log(tool);
+    var x = ev.clientX;
+    
+    if (ev.type === 'mouseover') {
+      showTriggerSelect(element);
+    } else if (ev.type === 'mouseleave') {
+      hideTriggerSelect(element);
+    }
+
+    if (x >= 380) {
+      BoardData.setCurrentToolName(tool);
+    }
+  }
+
+  function showFirstLevel () {
+    $menuOpener.removeClass('show');
+    $firstLevelMenu.addClass('show');
+  }
+
+  function hideFirstLevel () {
+    $firstLevelMenu.removeClass('show');
+    $menuOpener.addClass('show');
+  }
+
   function showSecondLevel (child) {
     $secondLevelMenu.filter('#' + child).addClass('show');
   }
 
   function hideSecondLevel (child) {
-    $secondLevelMenu.filter('#' + child).removeClass('show'); 
+    if (child) {
+      $secondLevelMenu.filter('#' + child).removeClass('show'); 
+    } else {
+      $secondLevelMenu.removeClass('show');  
+    }
+  }
+
+  function showTriggerSelect (element) {
+    element.find('.trigger-select-tool').addClass('show');
+  }
+  
+  function hideTriggerSelect (element) {
+    element.find('.trigger-select-tool').removeClass('show');
   }
 
   return {
     setToolbarElements: setToolbarElements,
-    showFirstLevel: showFirstLevel,
-    secondLevelHandler: secondLevelHandler
+    firstLevelHandler: firstLevelHandler,
+    secondLevelHandler: secondLevelHandler,
+    selectToolHandler: selectToolHandler
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // var $toolbar;
-  // var $firstLevelMenu;
-  // var $secondLevelMenu;
-
-  // var levelDisplayed = null;
-
-
-  // function setToolbarElements (element) {
-  //   $toolbar = element;
-  //   $firstLevelMenu = element.find('.first-level');
-  //   $secondLevelMenu = element.find('.second-level');
-  // }
-
-
-  // function menuEventsDispatcher (ev) {
-  //   // get level displayed
-  //   // var levelDisplayed = getLevelDisplayed();
-  //   var x = ev.clientX;
-  //   console.log(ev)
-  //   // if none
-  //   if (levelDisplayed === null) {
-  //     // show first;
-  //     showFirstLevel();
-  //   } 
-  //   // if first
-  //   else if (levelDisplayed === 'first') {
-  //     // if x > 100
-  //     if (x > 100) {
-  //       // show second level
-  //       showSecondLevel();
-  //     }
-  //     // if x < 100 and second level is shown
-  //       // hide second level
-  //     // if x > 400
-  //       // hide all
-  //   }
-  //   // if second
-  //     // if x > 300
-  //       // show third
-  //     // if x < 300 and third level is shown
-  //       // hide third level
-  //     // if x > 600
-  //       // hide all
-  // }
-
-  // // function getLevelDisplayed () {
-  // //   return levelDisplayed;
-  // // }
-
-  // function setLevelDisplayed (level) {
-  //   levelDisplayed = level;
-  // }
-  // // function menuEventsDispatcher (ev) {
-  // //   var x = ev.clientX;
-  // //   if (x < 3) {
-  // //     // SHOW toolbar
-  // //     toolbarShow();
-  // //     // SHOW firstLevel
-
-  // //     //mouseEvents[ev.type](ev, 'firstLevel');
-  // //   } else if (x > 6) {
-  // //     // HIDE toolbar
-  // //     // HIDE firstLevel
-  // //   } else if (x > 300) {
-  // //     // SHOW thirdLevel
-  // //     // mouseEvents[ev.type](ev, 'secondLevel');
-  // //   } else if (x > 100) {
-  // //     // SHOW secondLevel
-  // //     // mouseEvents[ev.type](ev, 'secondLevel');
-  // //     // console.log('Open SECOND')
-  // //   } else if (x < 300) {
-  // //     // HIDE thirdLevel
-  // //   } else if (x < 100) {
-  // //     // HIDE secondLevel
-  // //     // mouseEvents[ev.type](ev, 'secondLevel');
-  // //   }
-  // // }
-
-  // function showFirstLevel () {
-  //   setLevelDisplayed('first');
-  //   toolbar('show');
-  //   firstLevel('show');
-  // }
-
-  // function showSecondLevel () {
-  //   setLevelDisplayed('second');
-  //   secondLevel('show');
-  // }
-
-  // function toolbar (action) {
-  //   $toolbar.addClass(action);
-  // }
-
-  // function firstLevel (action) {
-  //   $firstLevelMenu.addClass(action);
-  // }
-
-  // function secondLevel (action) {
-  //   $secondLevelMenu.addClass(action);
-  // }
-  // // function setMenuLevelStatus (level, status) {
-  // //   isOpen[level] = status;
-  // // }
-
-  // // function checkMenuStatus (level) {
-  // //   return isOpen[level];
-  // // }
-
-
-
-  // return {
-  //   setToolbarElements: setToolbarElements,
-  //   menuEventsDispatcher: menuEventsDispatcher
-  // };
-
-});
+}]);
